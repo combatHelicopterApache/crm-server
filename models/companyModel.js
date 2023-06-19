92f92c45010570575a428f71f52daa9e3e935366
combatHelicopterApache <combat.helicopter.apache.main@gmail.com>
2023-06-19 13:43:06 +0300

    add brand-list

--- models/companyModel.js
+++ models/companyModel.js
@@ -1,32 +1,35 @@
 const mongoose = require("mongoose");
 const {
-  model,
-  Schema,
-  Types: { ObjectId },
+    model,
+    Schema,
+    Types: {ObjectId},
 } = mongoose;
 
 const companyModel = new Schema(
-  {
-    company_name: { type: String, required: true, unique: false },
-    company_email: { type: String, required: true, unique: false },
-    company_phone: { type: String, required: true, unique: false },
-    admin_name: { type: String, required: true, unique: false },
-    admin_phone: { type: String, required: true, unique: false },
-    admin_email: { type: String, required: true, unique: false },
-    company_identifier: { type: String, required: true, unique: false },
-    address: { type: String, required: true, unique: false },
-    title: { type: String, required: false, unique: false },
-    notes: { type: String, required: false, unique: false },
-    status: { type: Number, required: true, unique: false },
-    owner_id: { type: String, required: false, unique: false },
-    owner: { type: ObjectId, ref: "Users", require: false },
-  },
-  {
-    timestamps: {
-      createdAt: "created_at",
-      updatedAt: "updated_at",
+    {
+        company_name: {type: String, required: true, unique: false},
+        company_email: {type: String, required: true, unique: false},
+        company_phone: {type: String, required: true, unique: false},
+        admin_name: {type: String, required: true, unique: false},
+        admin_phone: {type: String, required: true, unique: false},
+        admin_email: {type: String, required: true, unique: false},
+        company_identifier: {type: String, required: true, unique: false},
+        address: {type: String, required: true, unique: false},
+        title: {type: String, required: false, unique: false},
+        notes: {type: String, required: false, unique: false},
+        status: {type: Number, required: true, unique: false},
+        owner_id: {type: String, required: false, unique: false},
+        owner: {type: ObjectId, ref: "Users", require: false},
+        brands: [
+            { type: ObjectId, ref: "Brands", require: false }
+        ]
     },
-  }
+    {
+        timestamps: {
+            createdAt: "created_at",
+            updatedAt: "updated_at",
+        },
+    }
 );
 
 module.exports = model("Company", companyModel);
