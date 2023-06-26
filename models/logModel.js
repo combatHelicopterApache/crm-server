1d0b9b574fa36caa07206f35d66f65931e1838fb
combatHelicopterApache <combat.helicopter.apache.main@gmail.com>
2023-06-26 18:52:15 +0300

    add log model

---
+++ models/logModel.js
@@ -0,0 +1,29 @@
+const {model,
+    Schema,
+    Types: { ObjectId }
+} = require('mongoose')
+
+const log = new Schema({
+        user_id: {type: ObjectId, required: true},
+        user_name: {type: String, required: false},
+        action: {type: String, required: true, default: ''},
+        level: {type: String, required: false},
+        relation: {type: String, required: false, default: ''},
+        relation_id: {type: String, required: false, default: ''},
+        message: {type: String, required: false, default: ''},
+        created_by: {type: ObjectId, ref:'Users', required: false, default: ''},
+        updated_by: {type: ObjectId, ref:'Users', required: false, default: ''},
+        metadata: {
+            ip: {type: String, required: false, default: '0.0.0.0'},
+            domain: {type: String, required: false, default: ''},
+            app_version: {type: String, required: false, default: ''},
+        },
+    }, {
+        timestamps: {
+            createdAt: "createdAt",
+            updatedAt: "updatedAt",
+        }
+    }
+)
+
+module.exports = model('Logs', log)
