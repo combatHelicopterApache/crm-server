9884d97ef1e4d8ea7930de58a7dfc6865bb28a46
JoinToIt <maksym.kovalenko@jointoit.com>
2023-06-27 09:47:00 +0300

    aadd manager_id to office

--- models/officeModel.js
+++ models/officeModel.js
@@ -1,24 +1,29 @@
-const { model, Schema, Types: { ObjectId } } = require("mongoose");
+const {
+  model,
+  Schema,
+  Types: { ObjectId },
+} = require("mongoose");
 
 const officeModel = new Schema(
-    {
-        title: { type: String, required: false, default: "" },
-        address: { type: String, required: false, default: "" },
-        description: { type: String, required: false, default: "" },
-        company_id: { type: ObjectId, ref: "Companies", require: false },
-        active: { type: Boolean, required: false, default: true},
-        time_cards: {
-            type: Object,
-            required: false,
-            default: { time_start: "10:00", time_end: "19:00" },
-        },
+  {
+    title: { type: String, required: false, default: "" },
+    address: { type: String, required: false, default: "" },
+    description: { type: String, required: false, default: "" },
+    company_id: { type: ObjectId, ref: "Companies", require: false },
+    manager_id: { type: ObjectId, require: true },
+    active: { type: Boolean, required: false, default: true },
+    time_cards: {
+      type: Object,
+      required: false,
+      default: { time_start: "10:00", time_end: "19:00" },
     },
-    {
-        timestamps: {
-            createdAt: "created_at",
-            updatedAt: "updated_at",
-        },
-    }
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
 );
 
 module.exports = model("Offices", officeModel);
