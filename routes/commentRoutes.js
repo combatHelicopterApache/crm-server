ac00fad615be1b14a5f667b3e67534fbe2eb83ba
JoinToIt <maksym.kovalenko@jointoit.com>
2023-07-06 17:52:42 +0300

    fixed rout

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,30 +1,29 @@
-const {Router} = require('express')
+const { Router } = require("express");
 const commentController = require("../controllers/commentController");
-const checkAuthMiddleware = require('../middlewares/authMiddleware')
-const checkRoleMiddleware = require('../middlewares/roleMiddleware')
+const checkAuthMiddleware = require("../middlewares/authMiddleware");
+const checkRoleMiddleware = require("../middlewares/roleMiddleware");
 
-const router = Router()
+const router = Router();
 
 router.post(
-    '/:element/:id',
-    checkAuthMiddleware,
-    checkRoleMiddleware,
-    commentController.createNewComment
-)
+  "/:element/:id",
+  checkAuthMiddleware,
+  checkRoleMiddleware,
+  commentController.createNewComment
+);
 
 router.get(
-    '/:element/:id',
-    checkAuthMiddleware,
-    checkRoleMiddleware,
-    commentController.getCommentById
-)
+  "/:element/:id",
+  checkAuthMiddleware,
+  checkRoleMiddleware,
+  commentController.getCommentById
+);
 
 router.delete(
-    '/:id',
-    checkAuthMiddleware,
-    checkRoleMiddleware,
-    commentController.deleteCommentById
-)
+  "/:element/:id",
+  checkAuthMiddleware,
+  checkRoleMiddleware,
+  commentController.deleteCommentById
+);
 
-
-module.exports = router
+module.exports = router;
